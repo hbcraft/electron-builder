99aa82ec57acf4e180e459804f6869db3a8c9733
Mike Maietta <mmaietta@outlook.com>
2025-03-24 15:01:56 -0700

    "singleton" electron/get downloader to prevent collisions in cache

--- packages/app-builder-lib/src/util/electronGet.ts
+++ packages/app-builder-lib/src/util/electronGet.ts
@@ -3,6 +3,10 @@ import { getUserDefinedCacheDir, PADDING } from "builder-util"
 import * as chalk from "chalk"
 import { MultiProgress } from "electron-publish/out/multiProgress"
 
+
+const configToPromise = new Map<string, Promise<string>>()
+
+
 export type ElectronDownloadOptions = Omit<
   ElectronPlatformArtifactDetails,
   "platform" | "arch" | "version" | "artifactName" | "artifactSuffix" | "customFilename" | "tempDirectory" | "downloader" | "cacheMode" | "cacheRoot"
@@ -19,28 +23,42 @@ type ElectronGetDownloadConfig = {
 }
 
 export async function downloadArtifact(config: ElectronGetDownloadConfig, progress: MultiProgress | null) {
+  // Old cache is ignored if cache environment variable changes
+  const cacheDir = await getUserDefinedCacheDir()
+  const cacheName = `${cacheDir}${JSON.stringify(config)}`
+
+  let promise = configToPromise.get(cacheName) // if rejected, we will try to download again
+
+  if (promise != null) {
+    return promise
+  }
+
+  promise = doDownloadArtifact(config, cacheDir, progress)
+  configToPromise.set(cacheName, promise)
+  return promise
+}
+
+async function doDownloadArtifact(config: ElectronGetDownloadConfig, cacheDir: string | undefined, progress: MultiProgress | null) {
   const { electronDownload, arch, version, platformName, artifactName } = config
 
   const progressBar = progress?.createBar(`${" ".repeat(PADDING + 2)}[:bar] :percent | ${chalk.green(artifactName)}`, { total: 100 })
   progressBar?.render()
 
-  const cacheEnv = await getUserDefinedCacheDir()
-
+  const downloadOptions: GotDownloaderOptions = {
+    getProgressCallback: progress => {
+      progressBar?.update(progress.percent)
+      return Promise.resolve()
+    },
+  }
   const artifactConfig: ElectronPlatformArtifactDetails = {
-    cacheMode: cacheEnv ? ElectronDownloadCacheMode.ReadOnly : undefined,
-    cacheRoot: cacheEnv,
+    cacheMode: cacheDir ? ElectronDownloadCacheMode.ReadOnly : undefined,
+    cacheRoot: cacheDir,
     ...(electronDownload ?? {}),
     platform: platformName,
     arch,
     version,
     artifactName,
-    downloadOptions: {
-      getProgressCallback: progress => {
-        if (progressBar) {
-          progressBar.update(progress.percent)
-        }
-      },
-    } as GotDownloaderOptions,
+    downloadOptions,
   }
 
   const dist = await _downloadArtifact(artifactConfig)
