8f55851cad82ccb18ca4d023ae708e00d094c3bc
Mike Maietta <mmaietta@outlook.com>
2025-03-24 19:06:22 -0700

    remove cache dir logic

--- packages/app-builder-lib/src/util/electronGet.ts
+++ packages/app-builder-lib/src/util/electronGet.ts
@@ -1,5 +1,5 @@
-import { downloadArtifact as _downloadArtifact, ElectronDownloadCacheMode, ElectronPlatformArtifactDetails, GotDownloaderOptions, MirrorOptions } from "@electron/get"
-import { getUserDefinedCacheDir, PADDING } from "builder-util"
+import { downloadArtifact as _downloadArtifact, ElectronPlatformArtifactDetails, GotDownloaderOptions, MirrorOptions } from "@electron/get"
+import { PADDING } from "builder-util"
 import * as chalk from "chalk"
 import { MultiProgress } from "electron-publish/out/multiProgress"
 
@@ -21,9 +21,7 @@ type ElectronGetDownloadConfig = {
 }
 
 export async function downloadArtifact(config: ElectronGetDownloadConfig, progress: MultiProgress | null) {
-  // Old cache is ignored if cache environment variable changes
-  const cacheDir = await getUserDefinedCacheDir()
-  const cacheName = `${cacheDir}${JSON.stringify(config)}`
+  const cacheName = JSON.stringify(config)
 
   let promise = configToPromise.get(cacheName) // if rejected, we will try to download again
 
@@ -31,12 +29,12 @@ export async function downloadArtifact(config: ElectronGetDownloadConfig, progre
     return promise
   }
 
-  promise = doDownloadArtifact(config, cacheDir, progress)
+  promise = doDownloadArtifact(config, progress)
   configToPromise.set(cacheName, promise)
   return promise
 }
 
-async function doDownloadArtifact(config: ElectronGetDownloadConfig, cacheDir: string | undefined, progress: MultiProgress | null) {
+async function doDownloadArtifact(config: ElectronGetDownloadConfig, progress: MultiProgress | null) {
   const { electronDownload, arch, version, platformName, artifactName } = config
 
   const progressBar = progress?.createBar(`${" ".repeat(PADDING + 2)}[:bar] :percent | ${chalk.green(artifactName)}`, { total: 100 })
@@ -49,8 +47,6 @@ async function doDownloadArtifact(config: ElectronGetDownloadConfig, cacheDir: s
     },
   }
   const artifactConfig: ElectronPlatformArtifactDetails = {
-    cacheMode: cacheDir ? ElectronDownloadCacheMode.ReadOnly : undefined,
-    cacheRoot: cacheDir,
     ...(electronDownload ?? {}),
     platform: platformName,
     arch,
